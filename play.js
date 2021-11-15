a3855e4b7113bb20e195a4a75fc050cd7faf1ea0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-15 16:34:15 -0500

    establish the connection object

---
+++ play.js
@@ -0,0 +1,30 @@
+const net = require('net');
+//// establish a connectwith the game server ////
+
+//// create the connection in a function ////
+const connect = function () {
+  conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  })
+  
+  //// Guard: interpret incoming data as test ////
+  conn.setEncoding('utf8')
+  conn.on('data', data => {
+    console.log('Server said: ', data);
+  })
+  //// returns the connection as an object ////
+  return conn; 
+}
+
+console.log('Connecting ... ...');
+//|| call the connection object ||//
+connect();
+
+
+// ================================================
+// Establishing a connection:
+// ================================================
+// 1. Use Node [net] library with the [createConnnection] method --> object called "conn"
+// 2. th conn object that Node returned represeents the connection with the server
+// 3. now use the methods of the "conn" object to interact with the server 
