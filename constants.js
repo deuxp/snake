a7dc9cec2eecb521f70ba5b8c105690bf5c33182
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-16 15:39:40 -0500

    fix a bug in NAME, incorrect syntax

--- constants.js
+++ constants.js
@@ -1,8 +1,26 @@
 const IP = 'localhost';
 const PORT = 50541;
+const NAME = 'Name: RAT' // max 3 characters
+
+const KEYMAP = {
+
+  // movement keys
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+
+// canned messages
+  t: 'Say: hadouken!',
+  y: 'Say: Ssssssss',
+  u: 'Say: sonic boom!',
+
+};
 
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  NAME,
+  KEYMAP,
 };
