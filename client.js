07cdec5028789e94e9132bee3415d8912efdb1a1
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-16 15:41:45 -0500

    add descriptive comment

--- client.js
+++ client.js
@@ -1,5 +1,14 @@
+/** 
+ ================================================
+ Establishing a connection:
+ ================================================
+ 1. Use Node [net] library with the [createConnnection] method --> object called "conn"
+ 2. the conn object that Node returned represeents the connection with the server
+ 3. now use the methods of the "conn" object to interact with the server
+ */
+ 
 const net = require('net');
-const { IP, PORT } = require('./constants');
+const { IP, PORT, NAME } = require('./constants');
 //// establish a connectwith the game server ////
 
 //// create the connection in a function ////
@@ -12,12 +21,12 @@ const connect = function() {
   //// Guard: interpret incoming data as test ////
   conn.setEncoding('utf8');
   conn.on('data', data => {
-    console.log('Server said: ', data);
+    console.log('Gottfried, Server said: ', data);
   });
 
   conn.on('connect', () => {
     console.log(`TCP: connection established`);
-    conn.write('Name: RAT'); // <-- this is sent as soon as the connection is established
+    conn.write(NAME); // <-- this is sent as soon as the connection is established
   });
 
   return conn;
@@ -25,10 +34,3 @@ const connect = function() {
 module.exports = {
   connect,
 };
-
-// ================================================
-// Establishing a connection:
-// ================================================
-// 1. Use Node [net] library with the [createConnnection] method --> object called "conn"
-// 2. the conn object that Node returned represeents the connection with the server
-// 3. now use the methods of the "conn" object to interact with the server
--- play.js
+++ play.js
@@ -1,22 +1,20 @@
 const { setupInput } = require('./input');
 const { connect } = require('./client');
 
-
-// ================================================
-// THE CLIENT MODULE
-// 1. the connect() function returns an object that
-//    allows us to interact with the server [imported]
-
-
-// THE INPUT MODULE --> setting up the stdin
-// 2. the stdin object returned by setupInput() will
-//    listen for keyboard input and react to it.
-// 3. the function, handleUserInput() will determine the
-//    behaviour of key presses
-// ================================================
+/**
+ ================================================
+ THE CLIENT MODULE
+ 1. the connect() function returns an object that
+    allows us to interact with the server [imported
+ THE INPUT MODULE --> setting up the stdin
+ 2. the stdin object returned by setupInput() will
+    listen for keyboard input and react to it.
+ 3. the function, handleUserInput() will determine the
+    behaviour of key presses
+ ===============================================
+ */
 
 console.log('Connecting ... ...');
-
 
 //|\ call the input module -> pass it the connection object, to be used in the global scope of the input module by reassigning it to a global variable declared as undefined from within said function.
 // now that connection object can be used from the input module, which means that all of the connection object methods are available on that page and can be accessed by the behaviour of the stdin input evironment or listeners.
