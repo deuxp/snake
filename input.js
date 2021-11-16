a08f4a8b7322d4f37a3ae3113aa62772db689ad2
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-16 15:41:14 -0500

    add a truthy guard against random key press

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { KEYMAP } = require('./constants')
+
 let connection; // default undefined -> gets passed the connection from the function call on the other page and put in this global scope.. clever.
 
 //// Object Listens for keyboard input ////
@@ -9,27 +11,21 @@ const setupInput = function(conn) {
   stdin.resume();
   //|\ register an event listener for stdin with the .on method
   // if there is a .on method that means that object is listening for a registered event.
-  stdin.on('data', handleUserInput); // must be in our stdin object.. the function call defines the action taken once the event is registered.
+  stdin.on('data', handleUserInput); // STREAMING IN: must be in our stdin object.. the function call defines the action taken once the event is registered.
   return stdin;
 };
 
 const handleUserInput = key => {
-  // needs to check for ctrl+c
+  // Exit: check for ctrl+c
   if (key === '\u0003') {
-    console.log('you exited with the exit key');
     process.exit();
+  };
+  /// Movement keys: w,a,s,d /// Canned message: t,y,u ///
+  if (KEYMAP[key]) { /// Guard: against random key press crashing the game
+    connection.write(KEYMAP[key]);
   }
-  // movement keys
-  if (key === 'w') connection.write('Move: up');
-  if (key === 'a') connection.write('Move: left');
-  if (key === 's') connection.write('Move: down');
-  if (key === 'd') connection.write('Move: right');
-  // msg in a can
-  if (key === 't') connection.write('Say: hadouken!');
-  if (key === 'y') connection.write('Say: Ssssssss');
-  if (key === 'u') connection.write('Say: sonic boom!');
-
 };
+
 
 module.exports = {
   setupInput,
