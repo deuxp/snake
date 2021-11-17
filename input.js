96909c34b688c4642a311e2a5942c8a363ab1103
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-16 20:12:17 -0500

    correct comments in code

--- input.js
+++ input.js
@@ -1,16 +1,16 @@
 const { KEYMAP } = require('./constants');
 
-let connection; // default undefined -> gets passed the connection from the function call on the other page and put in this global scope.. clever.
+let connection; // default undefined -> gets passed the connection object from the setupInput() call from the play.js file --> assigned to variable in the global scope.. clever.
 
-//// Object Listens for keyboard input ////
+//// Returns an object that listens for keyboard input ////
 const setupInput = function(conn) {
   connection = conn; // the connection module passed here from the play.js file is reassigned tot he global scop of this file; ready to be used by any function here.. including the key event handler . wink wink
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  //|\ register an event listener for stdin with the .on method
-  // if there is a .on method that means that object is listening for a registered event.
+  //|\ register an event listener for stdin with the `.on` method
+  // if there is a `.on` method that means that object is listening for a registered event.
   stdin.on('data', handleUserInput); // STREAMING IN: must be in our stdin object.. the function call defines the action taken once the event is registered.
   return stdin;
 };
