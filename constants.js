e850da744a7c237b37f1c5be48c58ab97e018d69
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-21 23:32:16 -0500

    add LASTKEY constant to guard against backwards keypress

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+const { stdin } = require("process");
+
 const IP = 'localhost';
 const PORT = 50541;
 const NAME = 'Name: RAT'; // max 3 characters
@@ -9,7 +11,7 @@ const KEYMAP = {
   a: 'Move: left',
   s: 'Move: down',
   d: 'Move: right',
-
+  
   // canned messages
   t: 'Say: hadouken!',
   y: 'Say: Ssssssss',
@@ -18,9 +20,21 @@ const KEYMAP = {
 };
 
 
+const LASTKEY = {
+
+  // last key CONST is bound to its 180 degree counterpart and checked before movement, since only 90 degree turns are allowed.
+  w: 's',
+  a: 'd',
+  s: 'w',
+  d: 'a'
+  
+}
+
+
 module.exports = {
   IP,
   PORT,
   NAME,
   KEYMAP,
+  LASTKEY,
 };
